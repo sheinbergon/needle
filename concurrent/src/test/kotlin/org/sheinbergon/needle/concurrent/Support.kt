875e44d721db4a4425b0eb656b171a3078ec138a
Idan Sheinberg <ishinberg0@gmail.com>
2020-09-11 16:16:44 +0300

    ForkJoinPool pinning support added to both core and concurrent sub-modules Started working on test coverage

--- concurrent/src/test/kotlin/org/sheinbergon/needle/concurrent/Support.kt
+++ concurrent/src/test/kotlin/org/sheinbergon/needle/concurrent/Support.kt
@@ -1,6 +1,7 @@
 package org.sheinbergon.needle.concurrent
 
-import org.amshove.kluent.*
+import org.amshove.kluent.shouldBe
+import org.amshove.kluent.shouldBeEqualTo
 import org.sheinbergon.needle.PinnedThread
 import org.sheinbergon.needle.binaryTestMask
 import org.sheinbergon.needle.testAffinityDescriptor
@@ -8,18 +9,19 @@ import org.sheinbergon.needle.textTestMask
 import java.util.concurrent.Callable
 import java.util.concurrent.CountDownLatch
 import java.util.concurrent.Executors
-
+import java.util.concurrent.ForkJoinPool
 
 internal const val SCHEDULING_DELAY = 500L
 
 internal object TestMaskPinnedThreadFactory : PinnedThreadFactory {
     override fun newThread(r: Runnable) = PinnedThread(r, testAffinityDescriptor)
+
+    override fun newThread(pool: ForkJoinPool) = PinnedThread.ForkJoinWorker(pool, testAffinityDescriptor)
 }
 
 @Suppress("UNCHECKED_CAST")
 internal fun callableTask(latch: CountDownLatch, visited: MutableSet<PinnedThread>): Callable<Unit> =
-        Executors.callable { runnableTask(latch, visited).run() } as Callable<Unit>
-
+    Executors.callable { runnableTask(latch, visited).run() } as Callable<Unit>
 
 internal fun runnableTask(latch: CountDownLatch, visited: MutableSet<PinnedThread>) = Runnable {
     val self = Thread.currentThread() as PinnedThread
